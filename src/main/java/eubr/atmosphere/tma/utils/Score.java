8901d62558834b727107c3fb5d98d0cb91c0e22c
Jose Pereira <josep@dei.uc.pt>
2018-09-28 15:13:30 +0100

    Initial commit

---
+++ src/main/java/eubr/atmosphere/tma/utils/Score.java
@@ -0,0 +1,70 @@
+package eubr.atmosphere.tma.utils;
+
+public class Score {
+
+    private Double cpuPod;
+    private Double memoryPod;
+    private Double cpuNode;
+    private Double memoryNode;
+    private Double score;
+
+    public Double getCpuPod() {
+        return cpuPod;
+    }
+
+    public void setCpuPod(Double cpuPod) {
+        this.cpuPod = cpuPod;
+    }
+
+    public Double getMemoryPod() {
+        return memoryPod;
+    }
+
+    public void setMemoryPod(Double memoryPod) {
+        this.memoryPod = memoryPod;
+    }
+
+    public Double getCpuNode() {
+        return cpuNode;
+    }
+
+    public void setCpuNode(Double cpuNode) {
+        this.cpuNode = cpuNode;
+    }
+
+    public Double getMemoryNode() {
+        return memoryNode;
+    }
+
+    public void setMemoryNode(Double memoryNode) {
+        this.memoryNode = memoryNode;
+    }
+    
+    @Override
+    public String toString() {
+        return "Score [cpuPod: " + this.getCpuPod() +
+              ", memoryPod: " + this.getMemoryPod() +
+              ", cpuNode: " + this.getCpuNode() +
+              ", memoryNode: " + this.getMemoryNode() + "]";
+    }
+
+    public Double getScore() {
+        Double a1 = this.getCpuPod() / this.getCpuNode();
+        Double a2 = this.getMemoryPod() / this.getMemoryNode();
+        this.score = 0.5 * a1 + 0.5 * a2;
+        return this.score;
+    }
+
+    public String getCsvLine() {
+        return this.getCpuPod().toString() +
+            "," + this.getMemoryPod().toString() +
+            "," + this.getCpuNode().toString() +
+            "," + this.getMemoryNode().toString() +
+            "," + this.getScore().toString();
+    }
+
+    public boolean isValid() {
+         return this.getCpuNode()!= null && this.getCpuPod()!= null &&
+                 this.getMemoryNode()!= null && this.getMemoryPod()!= null;
+    }
+}

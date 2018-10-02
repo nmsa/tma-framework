cd263055fb3232192affc8be6f0e7048bb4e0c23
Jose Pereira <josep@dei.uc.pt>
2018-10-02 16:04:57 +0100

    Adding a constructor to the configuration class

--- src/main/java/eubr/atmosphere/tma/data/Configuration.java
+++ src/main/java/eubr/atmosphere/tma/data/Configuration.java
@@ -5,6 +5,11 @@ public class Configuration {
     private String keyName;
     private String value; // This field is different from the one of the database
 
+    public Configuration(String keyName, String value) {
+        this.keyName = keyName;
+        this.value = value;
+    }
+
     public String getKeyName() {
         return keyName;
     }

5d1b3e7e8896b22e25f5ffa591717df9e7e6eb15
Rui Silva <rslv560@gmail.com>
2019-03-14 10:32:24 +0000

    Update Library Code

--- common/tmalibrary/setup.py
+++ common/tmalibrary/setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='tmalibrary',
-    version='0.2',
-    packages=['tmalibrary'],
-    url='https://github.com/rfrsilva/tmalibrary',
+    version='0.8',
+    packages=find_packages(),
+    url='https://github.com/eubr-atmosphere/tma-framework/tree/dev/common/common/tmalibrary',
     license='Apache 2.0',
     author='Rui Silva',
     author_email='rfsilva@student.dei.uc.pt',
---
+++ common/tmalibrary/tmalibrary/probes/__init__.py
@@ -0,0 +1,5 @@
+from communication import Communication
+from data import Data
+from message import Message
+from message import ComplexEncoder
+from observation import Observation

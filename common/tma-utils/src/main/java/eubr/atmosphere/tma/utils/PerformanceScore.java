22d92f6df839b4940049d1599bd5a062a0a2a70c
Jose Pereira <josep@dei.uc.pt>
2019-04-03 15:43:56 +0100

    Performance Quality Model changed to normalization approach to z-normalization

--- common/tma-utils/src/main/java/eubr/atmosphere/tma/utils/PerformanceScore.java
+++ common/tma-utils/src/main/java/eubr/atmosphere/tma/utils/PerformanceScore.java
@@ -5,6 +5,12 @@ public class PerformanceScore implements Score {
     public static final Double MAX_THROUGHPUT = 575.00;
     public static final Double MAX_RESPONSE_TIME = 150.00;
 
+    // Values used for the z-normalization
+    public static final Double MEAN_THROUGHPUT = 206.4624;
+    public static final Double STD_DEV_THROUGHPUT = 197.0407;
+    public static final Double MEAN_RESPONSE_TIME = 38.6027;
+    public static final Double STD_RESPONSE_TIME = 209.7015;
+
     private Double score;
     private Double throughput;
     private Double responseTime;
@@ -39,6 +45,14 @@ public class PerformanceScore implements Score {
 
     @Override
     public Double getScore() {
+        Double a5 = (this.getThroughput() - MEAN_THROUGHPUT) / STD_DEV_THROUGHPUT;
+        Double a6 = (this.getResponseTime() - MEAN_RESPONSE_TIME) / STD_RESPONSE_TIME;
+        this.score = 0.5 * a5 - 0.5 * a6;
+        return this.score;
+    }
+
+    // this is the old version, which uses the maximum values to normalize.
+    public Double getScoreMinMaxNormalization() {
         Double a5 = this.getThroughput() / MAX_THROUGHPUT;
         Double a6 = this.getResponseTime() / MAX_RESPONSE_TIME;
 

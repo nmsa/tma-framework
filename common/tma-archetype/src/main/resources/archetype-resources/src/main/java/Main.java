d695a5678fa34fddd91ec499f1a9233a5d0c9c65
Nuno Antunes <nmsa@dei.uc.pt>
2019-05-06 08:42:25 +0100

    Knowledge DB and D3.5 diagrams (#11)
    * Final Diagrams for D3.5
    
    * TMA Framework Modules updated.
    
    * Class diagram of the probe library (java)
    * Updated the Actuator Class Diagram
    * Libraries (Java and Python) (#12)
    * Maven archetype moved from tma-framework-m.
    
    * Changes performed in the database.
    - Rename: MetricComponent -> LeafAttribute
    - Rename: MetricComposition -> CompositeAttribute
    - Rename: add attribute "attributeAggregationOperator" in the
             "CompositeAttribute" entity
    - [Entity: LeafAttribute] Rename attributeAggregationOperator
             -> metricAggregationOperator
    - [Entity: LeafAttribute] Add the attributes normalizationKind
             and normalizationMetric
    - [Entity: Metric] Remove normalizationKind attribute
    - [Entity: QualityModel] Add "businessThreshold" attribute
    - Rename "threshold" attribute from "Metric" to "normalizationThreshold"
    - Updating type of attribute attributeAggregationOperator (Entity CompositeAttribute)

---
+++ common/tma-archetype/src/main/resources/archetype-resources/src/main/java/Main.java
@@ -0,0 +1,36 @@
+/**
+ * <b>ATMOSPHERE</b> - http://www.atmosphere-eubrazil.eu/
+ * ***
+ * <p>
+ * <b>Trustworthiness Monitoring & Assessment Framework</b>
+ * Component: // FIXME: Define associated component
+ * <p>
+ * Repository: https://github.com/eubr-atmosphere/tma-framework
+ * License: https://github.com/eubr-atmosphere/tma-framework/blob/master/LICENSE
+ * <p>
+ * <p>
+ */
+package $package;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Please, add a short description of the class.
+ * <p>
+ *
+ * @author Nuno Antunes <nmsa@dei.uc.pt>
+ */
+public class Main {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    /**
+     * Please, document your main methods using <tt>Javadoc</tt>.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        LOGGER.info("This is the main class of the project. Edit as necessary.");
+    }
+}

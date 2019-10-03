9fbcb6a0755984e8c5ba07b0028817ad354ab2f6
Rui Silva <rslv560@gmail.com>
2019-10-03 10:56:47 +0100

    Add Class to Calculate Score of CloudEA Quality Model

---
+++ common/tma-utils/src/main/java/eubr/atmosphere/tma/utils/SecurityCloudEAScore.java
@@ -0,0 +1,30 @@
+package eubr.atmosphere.tma.utils;
+
+public class SecurityCloudEAScore implements Score {
+
+    // metrics used to calculte QQM
+
+	private Double score;
+	// Constructor of SecurityCloudEAScore 
+    public SecurityCloudEAScore() {
+        super();
+        // initialization of this class variables
+    }
+
+   
+
+    // get and set methods for each class variable
+
+
+    @Override
+    public String toString() {
+        return //return string with values of class variables
+    }
+
+    @Override
+    public Double getScore() {
+        // operations to calcute score
+        
+        return this.score;
+    }
+}
